a7ad266eb927522818689afebfecc775b8dda52d
Gavin Lucas <Gavin@L337.org>
2026-03-31 10:59:14 +0100

    Add additional error handling to speedtest collector

--- toinflux/speedtest.py
+++ toinflux/speedtest.py
@@ -5,6 +5,8 @@ __copyright__ = "Copyright (C) 2025 Gavin Lucas"
 __license__ = "MIT License"
 __version__ = "1.0"
 
+import sys
+import socket
 import speedtest
 from toinflux.influx import DataHandler
 
@@ -19,21 +21,31 @@ class Speedtest(DataHandler):
 
     def get_data(self):
         """Run and get the data from Speedtest"""
-        st = speedtest.Speedtest()
+        try:
+            st = speedtest.Speedtest(timeout=self.settings["speedtest"].get("timeout", 120))
 
-        # run the download test
-        st.download()
+            # run the download test
+            st.download()
 
-        # run the upload test
-        st.upload()
+            # run the upload test
+            st.upload()
 
-        # get the results
-        st_data = st.results.dict()
+            # get the results
+            st_data = st.results.dict()
+        except speedtest.SpeedtestException as e:
+            print(f"Error running Speedtest - {e}")
+            sys.exit(2)
+        if not isinstance(st_data, dict):
+            print("Error running Speedtest - invalid results")
+            sys.exit(2)
 
         # just extract the specific fields we want here
         if "fields" in self.settings["speedtest"]:
             self.data = {k: st_data[k] for k in self.settings["speedtest"]["fields"] if k in st_data}
         else:
             self.data = st_data
+
+        # use the local hostname as the host tag
+        self.influx_header = f"speedtest,host={socket.gethostname().split(".")[0]} "
 
         return self.data
